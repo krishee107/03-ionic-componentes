bc018f595d9c1568559c50e85560a6f3f2b03aa6
krishee107 <krishee107@gmail.com>
2024-06-08 17:53:31 +0200

    Alerts

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -6,10 +7,112 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert.page.scss'],
 })
 export class AlertPage implements OnInit {
+  onClick() {
+    this.presentAlert();
+  }
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'A Short Title Is Best',
+      subHeader: 'A Sub Header Is Optional',
+      message: 'A message should be a short, complete sentence.',
+      buttons: ['Action'],
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertController.create({
+      header: 'A Short Title Is Best',
+      subHeader: 'A Sub Header Is Optional',
+      message: 'A message should be a short, complete sentence.',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            console.log('Ok clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Placeholder 1',
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: 'hello',
+          placeholder: 'Placeholder 2',
+        },
+        {
+          name: 'name3',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Favorite site ever',
+        },
+        {
+          name: 'name4',
+          type: 'text',
+          placeholder: 'Placeholder 4',
+        },
+        {
+          name: 'name5',
+          type: 'date',
+          placeholder: 'Placeholder 5',
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          //disabled
+          min: -5,
+          placeholder: 'Placeholder 6',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          },
+        },
+        {
+          text: 'Ok',
+          handler: (data) => {
+            console.log(data);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
   }
 
 }
