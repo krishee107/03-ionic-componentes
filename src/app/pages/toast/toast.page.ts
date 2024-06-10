341216c3697989fe61360ea19bd09167cc6e2587
krishee107 <krishee107@gmail.com>
2024-06-10 22:23:36 +0200

    Toast

---
+++ src/app/pages/toast/toast.page.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-toast',
+  templateUrl: './toast.page.html',
+  styleUrls: ['./toast.page.scss'],
+})
+export class ToastPage implements OnInit {
+
+  constructor(private toastController: ToastController) { }
+
+  ngOnInit() {
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    await toast.present();
+  }
+
+
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Hello World!',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+
+}
