eb0def64ccc2d868138cf77a51d91de996b72577
krishee107 <krishee107@gmail.com>
2024-06-10 19:07:47 +0200

    Menu

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componente } from 'src/app/interfaces/interfaces';
+import { DataService } from 'src/app/services/data.service';
 
-interface Componente {
-  icon: string;
-  name: string;
-  redirectTo: string;
-}
+
 
 @Component({
   selector: 'app-home',
@@ -12,89 +12,18 @@ interface Componente {
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  componentes: Componente[] = [
-    {
-      icon: 'american-football-outline',
-      name: 'Action Sheet',
-      redirectTo: '/action-sheet'
-    },
-    {
-      icon: 'logo-apple',
-      name: 'Alert',
-      redirectTo: '/alert'
-    },
-    {
-      icon: 'beaker-outline',
-      name: 'Avatar',
-      redirectTo: '/avatar'
-    },
-    {
-      icon: 'radio-button-off-outline',
-      name: 'Buttons',
-      redirectTo: '/buttons'
-    },
-    {
-      icon: 'card-outline',
-      name: 'card',
-      redirectTo: '/card'
-    },
-    {
-      icon: 'checkmark-circle-outline',
-      name: 'Checkbox',
-      redirectTo: '/checkbox'
-    },
-    {
-      icon: 'calendar-outline',
-      name: 'Datetime',
-      redirectTo: '/date-time'
-    }, {
-      icon: 'car-outline',
-      name: 'fab',
-      redirectTo: '/fab'
-    }, {
-      icon: 'grid-outline',
-      name: 'grid',
-      redirectTo: '/grid'
-    }, {
-      icon: 'infinite-outline',
-      name: 'infinite',
-      redirectTo: '/infinite'
-    },
-    {
-      icon: 'hammer-outline',
-      name: 'Input',
-      redirectTo: '/input'
-    },
-    {
-      icon: 'list-outline',
-      name: 'List sliding',
-      redirectTo: '/list'
-    },
-    {
-      icon: 'reorder-four-outline',
-      name: 'List Reorder',
-      redirectTo: '/list-reorder'
-    },
-    {
-      icon: 'refresh-circle-outline',
-      name: 'Loading',
-      redirectTo: '/loading'
-    },
-    {
-      icon: 'swap-vertical',
-      name: 'Modal',
-      redirectTo: '/modal'
-    },
-    {
-      icon: 'push-outline',
-      name: 'Popover',
-      redirectTo: '/popover'
-    },
-  ];
+  componentes!: Observable<Componente[]>;
 
-  constructor() { }
+  constructor(private menuCtrl: MenuController, private dataService: DataService) { }
 
   ngOnInit() {
+    this.componentes = this.dataService.getMenuOpts();
   }
+
+
+  // mostrarMenu() { // Funcion para mostrar el menu si no usamos el boton de menu imenubutton
+  //   console.log('Mostrar menu');
+  //   this.menuCtrl.open('first');
+  // }
 
 }
